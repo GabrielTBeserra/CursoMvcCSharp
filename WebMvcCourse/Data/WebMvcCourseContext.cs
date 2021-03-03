4fbfb6d97f2231f9f335c362256bc6af9541d9df
Gabriel Teles Beserra <gabrieltbeserra@gmail.com>
2021-03-03 19:17:38 -0300

    first commit

---
+++ WebMvcCourse/Data/WebMvcCourseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMvcCourse.Models;
+
+namespace WebMvcCourse.Data
+{
+    public class WebMvcCourseContext : DbContext
+    {
+        public WebMvcCourseContext (DbContextOptions<WebMvcCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMvcCourse.Models.Department> Department { get; set; }
+    }
+}
---
+++ WebMvcCourse/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebMvcCourse.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
