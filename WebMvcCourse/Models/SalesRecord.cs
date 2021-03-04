6dc4947710dd96658761eba511f6cf79103e9c9f
Gabriel Teles Beserra <gabrieltbeserra@gmail.com>
2021-03-04 18:49:51 -0300

    Other entities and second migration

--- WebMvcCourse/Models/SalesRecord.cs
+++ WebMvcCourse/Models/SalesRecord.cs
@@ -11,5 +11,17 @@ namespace WebMvcCourse.Models
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SalesRecord Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesRecord status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
     }
 }
