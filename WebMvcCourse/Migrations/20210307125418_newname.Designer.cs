b84c5f758bdf39083f2b9c9006aca138396bdb4c
Gabriel Teles Beserra <gabrieltbeserra@gmail.com>
2021-03-07 10:30:15 -0300

    SellersController

--- WebMvcCourse/Migrations/20210307125418_newname.Designer.cs
+++ WebMvcCourse/Migrations/20210307125418_newname.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMvcCourse.Data;
 
 namespace WebMvcCourse.Migrations
 {
     [DbContext(typeof(WebMvcCourseContext))]
-    partial class WebMvcCourseContextModelSnapshot : ModelSnapshot
+    [Migration("20210307125418_newname")]
+    partial class newname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebMvcCourse/Migrations/20210307125418_newname.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMvcCourse.Migrations
+{
+    public partial class newname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecords_SalesRecords_StatusId",
+                table: "SalesRecords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SalesRecords_StatusId",
+                table: "SalesRecords");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "SalesRecords");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SalesRecords",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SalesRecords");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "SalesRecords",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecords_StatusId",
+                table: "SalesRecords",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecords_SalesRecords_StatusId",
+                table: "SalesRecords",
+                column: "StatusId",
+                principalTable: "SalesRecords",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
